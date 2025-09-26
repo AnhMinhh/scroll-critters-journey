a5c5dad84bb18a074c8023d2898b0a26c8f38387
AnhMinhh <anhminh136005@gmail.com>
2025-09-26 18:46:17 +0700

    fix_loi_code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./components/ui/toaster";
+import { Toaster as Sonner } from "sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Topic from "./pages/Topic";
 import Posts from "./pages/Posts";
@@ -22,7 +23,7 @@ const App = () => (
           <Route path="/topic/:id" element={<Topic />} />
           <Route path="/posts/:category" element={<Posts />} />
           <Route path="/post/:id" element={<PostDetail />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* Catch-all route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
